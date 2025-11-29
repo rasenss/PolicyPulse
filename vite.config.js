2f014ab185471bf82a6bffc26954c6feeef3c3ff
rasendkhansa <rasuen27@gmail.com>
2025-11-29 09:11:43 +0700

    main: fix public/build

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    build: {
-        outDir: 'dist', 
-    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
