b5b3ead0ff07b454a4e966d96dcd92aa29917690
rasendkhansa <rasuen27@gmail.com>
2025-11-30 11:50:49 +0700

    main: fixed tab header logo

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PolicyPulse | Education Platform</title>
-    <link rel="icon" href="{{ asset('2.png') }}">
+    <link rel="icon" type="image/png" href="{{ asset('2.png') }}?v=1">
+    <link rel="shortcut icon" type="image/png" href="{{ asset('2.png') }}?v=1">
+    <link rel="apple-touch-icon" href="{{ asset('2.png') }}?v=1">
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     
