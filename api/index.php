5e8c5420365e0e192eb7530e45dd5238b4902e14
rasendkhansa <rasuen27@gmail.com>
2025-11-30 10:58:52 +0700

    deploy: try to fix balnk with upgraded vercel cersion

--- api/index.php
+++ api/index.php
@@ -1,7 +1,20 @@
 <?php
 
-// 1. Setup Vercel Environment agar Laravel tidak bingung
+// Paksa Storage ke /tmp (Karena Vercel Read-Only)
+$storagePath = '/tmp/storage';
+if (!is_dir($storagePath)) {
+    mkdir($storagePath, 0777, true);
+    mkdir($storagePath . '/framework/views', 0777, true);
+    mkdir($storagePath . '/framework/cache', 0777, true);
+}
+
+// Set environment variable
+putenv('APP_STORAGE=' . $storagePath);
+putenv('VIEW_COMPILED_PATH=' . $storagePath . '/framework/views');
+
+// Setup Vercel
 $_SERVER['SCRIPT_NAME'] = '/index.php';
 $_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/../public/index.php';
 
+// Jalankan Laravel
 require __DIR__ . '/../public/index.php';
