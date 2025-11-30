4e116e4e5e51c48aa7b29ffafaf818ab2248104e
rasendkhansa <rasuen27@gmail.com>
2025-11-30 11:08:33 +0700

    deploy: adding app key

--- api/index.php
+++ api/index.php
@@ -8,6 +8,8 @@ if (!is_dir($storagePath)) {
     mkdir($storagePath . '/framework/cache', 0777, true);
 }
 
+putenv('APP_KEY=base64:YQT2M6rOxvqFxU+xR7YrMsLzcW3rpEZ/uCiZ51s2894=');
+
 // Set environment variable
 putenv('APP_STORAGE=' . $storagePath);
 putenv('VIEW_COMPILED_PATH=' . $storagePath . '/framework/views');
